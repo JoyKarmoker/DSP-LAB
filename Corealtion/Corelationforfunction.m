94024cc7508ff9761afd29f8053cfd90bfc84b12
JoyKarmoker <Joykarmoker353@gmail.com>
2022-09-03 22:51:16 +0600

    Difference equation added

--- Corealtion/Corelationforfunction.m
+++ Corealtion/Corelationforfunction.m
@@ -8,7 +8,7 @@ t1 = -10:dt:10;
 x1 = sin(2*pi*0.05*t1);
 l1 = length(t1);
 
-t2 = -5:dt:5;
+t2 = -10:dt:5;
 x2 = sin(2*pi*0.2*t2);
 l2 = length(t2);
 
@@ -16,8 +16,8 @@ total_length = l1+l2-1;
 result = zeros(1, (total_length));
 kmin = -(total_length - l1);
 kmax = (total_length - l2);
-Nmin = t1(1)
-Nmax = t1(l1)
+Nmin = t1(1);
+Nmax = t1(l1);
 
 t3 = kmin:kmax;
 resultindex = 1;
@@ -26,11 +26,10 @@ for k = kmin:kmax
    for n = Nmin:Nmax
         x1index = FindIndex(t1, n);
         x2index = FindIndex(t2, (n-k));
-        if x1index ~= -1 && x2index ~= -1
-            result(resultindex) = result(resultindex) + (x1(x1index) * x2(x2index));
-            
+        if x2index ~= -1
+            result(resultindex) = result(resultindex) + (x1(x1index) * x2(x2index));       
         end
-       
+       n
    end
    resultindex = resultindex+1;
 end
@@ -38,4 +37,4 @@ end
 %figure(1)
 subplot(3,1,1), plot(t1, x1, 'r'), title('x1');
 subplot(3,1,2), plot(t2, x2, 'r'), title('x2');
-subplot(3,1,3), plot(t3, result, 'b'), title('corelation of x1 adn x2');
+subplot(3,1,3), plot(t3, result, 'b'), title('corelation of x1 and x2');
---
+++ Difference Equation/DifferenceEquation.m
@@ -0,0 +1,21 @@
+x = [0.4 0.9 1.2 1.3 3.4 0.7];
+xcof = [2/3, 1];
+ycof = [1, 0.5];
+output = zeros(1, length(x));
+
+for i=1:length(x)
+    for j = 2:length(ycof)
+        if i>=j
+            output(i) = output(i) + ycof(j)*output(i-j+1);
+        end     
+    end
+    
+     for j = 1:length(xcof)
+      if i >= j
+        output(i) = output(i) + xcof(j)*x(i-j+1);
+      end
+    end
+end
+t = 1: length(x);
+disp(output);
+plot(t,output, 'b'), title('difference equation'), xlabel('t'), ylabel('amplitude');
